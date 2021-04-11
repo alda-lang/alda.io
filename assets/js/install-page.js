b4098bfa208c3bf0447daa08a5b025251d4b2ed8
Dave Yarwood <dave.yarwood@gmail.com>
2021-04-11 16:57:21 -0400

    Overhaul /install page
    * Don't link to GitHub releases. Use the Alda releases API instead.
    * Include more information on this page instead of just linking to the README on
      GitHub.
    * Link to pages in docs about MIDI soundfonts and editor plugins

---
+++ assets/js/install-page.js
@@ -0,0 +1,173 @@
+const downloadDiv = document.getElementById("download-alda");
+const installDiv = document.getElementById("install-alda");
+
+const windowsPathInfoUrl = "https://medium.com/@kevinmarkvi/how-to-add-executables-to-your-path-in-windows-5ffa4ce61a53";
+
+const linuxPathInfoUrl = "https://linuxize.com/post/how-to-add-directory-to-path-in-linux/";
+
+function element(type, {innerHTML, innerText} = {}) {
+  const element = document.createElement(type);
+
+  if (innerHTML) {
+    element.innerHTML = innerHTML;
+  }
+
+  if (innerText) {
+    element.innerText = innerText;
+  }
+
+  return element;
+}
+
+downloadDiv.appendChild(element("em", {innerHTML: "Loading..."}));
+
+function bailOut() {
+  downloadDiv.innerHTML = `
+  <p>
+    <strong>Oops... something went wrong.</strong>
+
+    Please
+    <a href="mailto:dave.yarwood@gmail.com?subject=Alda+install+page">
+      send Dave an email
+    </a>
+
+    to give him a heads up!
+  </p>
+  `;
+}
+
+function unexpectedResponse(response) {
+  console.log("Unexpected response:", response);
+  bailOut();
+}
+
+const req = new XMLHttpRequest();
+req.addEventListener("load", function() {
+  let res;
+
+  try {
+    res = JSON.parse(this.responseText);
+  } catch (error) {
+    console.log(error);
+    return bailOut();
+  }
+
+  if (!res.releases || res.releases.length != 1) {
+    return unexpectedResponse(res);
+  }
+
+  const release = res.releases[0];
+
+  if (!release.version || !release.date || !release.assets) {
+    return unexpectedResponse(res);
+  }
+
+  // I'd rather use string interpolation here, but Vim doesn't syntax highlight
+  // it correctly, and I don't feel like digging into that at the moment.
+  document
+    .getElementById("step-2-download-alda")
+    .innerText =
+    `Step 2: Download Alda ${release.version} (${release.date})`;
+
+  // Alda v1 has only one executable, alda(.exe), whereas Alda v2 has two,
+  // alda(.exe) and alda-player(.exe). Here, we check the number of assets for
+  // one arbitrary OS-arch combination in the response, simply so that we can
+  // know whether or not to use "executables" in the plural in the instructions
+  // that we display on the page.
+  const [_, exampleAssets] = Object.entries(release.assets)[0];
+  const multipleExecutables = exampleAssets.length > 1;
+
+  downloadDiv.innerHTML = "";
+  downloadDiv.appendChild(
+    element(
+      "p",
+      {
+        innerText: multipleExecutables
+        ? "Download the Alda executables below for your platform:"
+        : "Download the Alda executable below for your platform:"
+      }
+    )
+  );
+
+  const table = element("table");
+
+  [
+    {label: "Mac (64-bit)",     key: "darwin-amd64"},
+    {label: "Windows (64-bit)", key: "windows-amd64"},
+    {label: "Windows (32-bit)", key: "windows-386"},
+    {label: "Linux (64-bit)",   key: "linux-amd64"},
+    {label: "Linux (32-bit)",   key: "linux-386"}
+  ].forEach(({label, key}) => {
+    const assets = release.assets[key];
+    if (!assets) return;
+
+    const assetsHTML =
+      assets.map(
+        ({name, url}) => `<a href="${url}">${name}</a>`
+      ).join(" + ");
+
+    table.appendChild(
+      element(
+        "tr",
+        {
+          innerHTML: `
+          <td><strong>${label}:</strong></td>
+          <td>${assetsHTML}</td>
+          `
+        }
+      )
+    );
+  });
+
+  downloadDiv.appendChild(table);
+
+  const files = multipleExecutables ? "files" : "file";
+  const filesAre = multipleExecutables ? "files are" : "file is";
+  const executables = multipleExecutables ? "executables" : "executable";
+  const executablesAre =
+    multipleExecutables ? "executables are" : "executable is";
+  const them = multipleExecutables ? "them" : "it";
+  const chmodTarget = multipleExecutables ? "{alda,alda-player}" : "alda";
+
+  installDiv.innerHTML = `
+  <p>
+    On Mac and Linux, first run <code>chmod +x ~/Downloads/${chmodTarget}</code>
+    (replace <code>~/Downloads</code> with wherever you downloaded the ${files})
+    to ensure that the ${filesAre} executable.
+  </p>
+
+  <p>
+    On Windows, the <code>.exe</code> ${filesAre} already executable, but you
+    may need to right-click the ${files} and click "unblock" in the file
+    properties.
+  </p>
+
+  <p>
+    Next, make the ${executables} available to run from the command line by
+    placing ${them} in a directory on your PATH.
+
+    <em>
+      (If you don't know what your PATH is, here is some information about PATH
+      for
+      <a href="${windowsPathInfoUrl}">Windows</a> or
+      <a href="${linuxPathInfoUrl}">Mac/Linux</a> users).
+    </em>
+  </p>
+  `
+
+  installDiv.appendChild(
+    element(
+      "p",
+      {
+        innerHTML: `
+        `
+      }
+    )
+  );
+})
+req.open("GET", "https://api.alda.io/releases/latest");
+req.send();
+
+// After downloading the `alda` (or `alda.exe`) program, follow [these instructions](https://github.com/alda-lang/alda#installation) to make it available for use in your terminal.
+
+
--- install.md
+++ install.md
@@ -5,10 +5,56 @@ subtitle: Available for Linux, macOS and Windows
 permalink: /install/
 ---
 
-`alda` (or `alda.exe` for Windows users) is a command-line program that you install on your `$PATH` and use in your terminal.
+Alda is [open source][alda-github] and available for free. You can run it from
+the command line on your Mac, Windows, or Linux computer.
 
-You can download the latest release [here on
-GitHub](https://github.com/alda-lang/alda/releases/latest).
+## Step 1: Install Java
 
-After downloading the `alda` (or `alda.exe`) program, follow [these instructions](https://github.com/alda-lang/alda#installation) to make it available for use in your terminal.
+Alda's sound engine requires Java to be installed.
 
+To verify whether or not you have Java installed, open a terminal and run `java
+-version`.
+
+If you don't have Java installed yet, we recommend downloading and installing
+the latest stable release from [AdoptOpenJDK].
+
+<!-- install-page.js depends on this ID. -->
+<h2 id="step-2-download-alda">Step 2: Download Alda</h2>
+
+<div id="download-alda"></div>
+
+## Step 3: Put it on your PATH
+
+<div id="install-alda"></div>
+
+## Try it out!
+
+In a new terminal, run these commands to verify that you've installed Alda
+correctly:
+
+{% highlight bash %}
+alda up
+alda version
+alda --help
+alda play -c '(tempo! 160) trumpet: (quant 60) f12 b- > d f6 d12 f1'
+{% endhighlight %}
+
+_If you're having trouble, please don't hesitate to ask for help in the [Alda
+Slack group][alda-slack]!_
+
+## Next steps
+
+Once you're up and running with Alda, we recommend:
+
+* Installing a good [MIDI soundfont][install-midi-soundfont]
+* Installing a [text editor plugin][editor-plugins]
+* Reading the [Alda tutorial][tutorial]
+
+<script src="{{ site.baseurl }}/assets/js/install-page.js"></script>
+
+[AdoptOpenJDK]: https://adoptopenjdk.net/
+[alda-github]: https://github.com/alda-lang/alda
+[alda-slack]: https://slack.alda.io
+[install-midi-soundfont]: https://github.com/alda-lang/alda/blob/master/doc/installing-a-good-soundfont.md
+[editor-plugins]: https://github.com/alda-lang/alda/blob/master/doc/editor-plugins.md
+[tutorial]: {{ site.baseurl }}/tutorial
